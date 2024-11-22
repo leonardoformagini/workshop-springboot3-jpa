d1d3d20d6f500750b653c019e7cbf5c0ff3101e4
Leonardo Junior Formagini <leonardoformagini@gmail.com>
2024-11-22 20:16:23 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -47,7 +47,7 @@ public class UserResource {
 	
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
-		service.delete(id);
+		service.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
 	
